87bffd0e3e3ae517deb1f80c132582ada784ad44
vysf <72449031+vysf@users.noreply.github.com>
2020-10-12 07:43:29 +0700

    commit kedua
    hapus komentar 2

--- run.py
+++ run.py
@@ -1,22 +1,6 @@
 from app import app
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'test'
-# mysql = MySQL(app)
-#mysql.init_app(app)
 
-# @app.route('/')
-# def index():
-# 	# cur = mysql.connection.cursor()
-# 	# #cur = mysql.get_db.cursor()
-# 	# cur.execute("SELECT * FROM flaskdb")
-# 	# rv = cur.fetchall()
-# 	# cur.close()
-	
-# 	# return render_template('home.html', computers=rv)
-#     return "Hello Yusuf hahah"
 
 if __name__=='__main__':
 	app.run(debug=True)
